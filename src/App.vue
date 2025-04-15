746e624c0eded7c0628061074cbbff2926a9b8d8
duasong111 <2272168170@qq.com>
2025-04-15 22:18:28 +0800

    初始化页面

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="wrapper">早上好啊</div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped></style>
