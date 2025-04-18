c1687dc8ec206ef47a189fbb6e430ff1bb7fbf23
duasong111 <2272168170@qq.com>
2025-04-18 08:00:01 +0800

    没有那么好看的一版，存在屏幕大小不适应的问题

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,13 @@
 
 <script setup lang="ts">
 import { defineComponent } from 'vue'
-import { useRouter } from 'vue-router' // 如果需要的话
+import { useRouter } from 'vue-router'
 </script>
 
 <style scoped>
 #app {
   width: 100%;
   height: 100%;
+  overflow: hidden; /* 防止滚动条 */
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './assets/main.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import autofit from 'autofit.js'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
@@ -11,4 +12,9 @@ const app = createApp(App)
 app.use(ElementPlus)
 app.use(pinia)
 app.use(router) // 使用路由
+// autofit.init({
+//   dw: 1920, // 设计稿宽度
+//   dh: 1080, // 设计稿高度
+//   el: '#app', // 缩放的根节点
+// })
 app.mount('#app')
